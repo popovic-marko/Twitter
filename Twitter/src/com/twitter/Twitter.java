3122cebe1424704f15a1a80be718864f8299dc09
popovic-marko <popovic.marko14@gmail.com>
2016-03-27 22:36:31 +0200

    Napisani testovi za klasu Twitter i unoceni nedostaci su ispravljeni.

--- Twitter/src/com/twitter/Twitter.java
+++ Twitter/src/com/twitter/Twitter.java
@@ -1,18 +1,15 @@
 package com.twitter;
-
+   
 import java.util.LinkedList;
 import com.twitter.poruke.TwitterPoruka;
-
 /**
  * Klasa koja sadrzi listu twitter poruka
  * @author Marko Popovic
  *
  */
 public class Twitter {
-
 	/** atribut koji predtavlja listu poruka */
-	private LinkedList<TwitterPoruka> poruke =
-			new LinkedList<TwitterPoruka>();
+	private LinkedList<TwitterPoruka> poruke = new LinkedList<TwitterPoruka>();
 	
 	/**
 	 * Vraca listu poruka koje ova klasa sadrzi
@@ -30,7 +27,7 @@ public class Twitter {
 	public void unesi(String korisnik, String poruka) {
 		//Pravi se nova poruka i puni podacima.
 		TwitterPoruka tp = new TwitterPoruka();
-		tp.setKorisnik("korisnik");
+		tp.setKorisnik(korisnik);
 		tp.setPoruka(poruka);
 		//Poruka se unosi u listu na kraj
 		poruke.addLast(tp);
@@ -45,14 +42,18 @@ public class Twitter {
 	public TwitterPoruka[] vratiPoruke(int maxBroj, String tag) {
 		if (tag==null || tag == "")
 			throw new RuntimeException("Morate uneti tag");
+	
 		//Ako je maxBroj <=0, vraca maxBroj se postavlja na 100 poruka
 		if (maxBroj<=0)
 			maxBroj = 100;
+		
 		//Pomocna promenljiva koja predstavlja brojac upisanih poruka
 		int brojac = 0;
+		
 		//Pomocni niz koja predstavlja rezultat pretrage tj. sadrzace
 		//sve poruke koje u sebi imaju zadati tag
 		TwitterPoruka[] rezultat = new TwitterPoruka[maxBroj];
+		
 		//Pretrazuju se poruke i traze se one koje sadrze tag.
 		//Ako se nadje neka takva, i ako nije prekoracen maxBroj
 		//ona se upisuje u niz. Ako je prekoracen maxBroj,pretraga
@@ -60,7 +61,7 @@ public class Twitter {
 		for (int i = 0; i < poruke.size(); i++)
 			if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
 				if (brojac < maxBroj){
-					rezultat[brojac+1]=poruke.get(i);
+					rezultat[brojac]=poruke.get(i);
 					brojac++;
 				}
 				else break;
---
+++ Twitter/src/test/com/twitter/TwitterTest.java
@@ -0,0 +1,67 @@
+package test.com.twitter;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.twitter.Twitter;
+import com.twitter.poruke.TwitterPoruka;
+
+public class TwitterTest {
+
+	private Twitter twitter;
+	
+	@Before
+	public void setUp() throws Exception {
+		twitter = new Twitter();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		twitter = null;
+	}
+
+	@Test
+	public void testUnesi(){
+		twitter.unesi("Marko", "Kako si?");
+		
+		assertEquals(1, twitter.vratiSvePoruke().size());
+		
+		assertEquals("Marko", twitter.vratiSvePoruke().get(0).getKorisnik());
+		assertEquals("Kako si?", twitter.vratiSvePoruke().get(0).getPoruka());
+	}
+	
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testVratiPorukeNull(){
+		twitter.vratiPoruke(10, null);
+	}
+
+	@Test (expected = java.lang.RuntimeException.class)
+	public void testVratiPorukePrazanString(){
+		twitter.vratiPoruke(10, "");
+	}
+	
+	@Test
+	public void testVratiPoruke(){
+		twitter.unesi("Marko", "@d Kako si?");
+		twitter.unesi("Nikola", "@d Danas je lep dan.");
+		
+		TwitterPoruka[] nizPoruka = twitter.vratiPoruke(2, "@d");
+		TwitterPoruka[] niz = new TwitterPoruka[2];
+		TwitterPoruka tp1 = new TwitterPoruka();
+		tp1.setKorisnik("Marko");
+		tp1.setPoruka("@d Kako si?");
+		niz[0] = tp1;
+		TwitterPoruka tp2 = new TwitterPoruka();
+		tp2.setKorisnik("Nikola");
+		tp2.setPoruka("@d Danas je lep dan.");
+		niz[1] = tp2;
+		
+		assertEquals(niz[0].getKorisnik(), nizPoruka[0].getKorisnik());
+		assertEquals(niz[0].getPoruka(), nizPoruka[0].getPoruka());
+		assertEquals(niz[1].getKorisnik(), nizPoruka[1].getKorisnik());
+		assertEquals(niz[1].getPoruka(), nizPoruka[1].getPoruka());
+	}
+}
