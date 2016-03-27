1f8e8107f3e8c85717115a0eb8cff42ca9405f54
popovic-marko <popovic.marko14@gmail.com>
2016-03-27 22:13:02 +0200

    Napisana JavaDoc dokumentacija za klasu TwitterPoruka, njene atribute i metode

--- Twitter/src/com/twitter/poruke/TwitterPoruka.java
+++ Twitter/src/com/twitter/poruke/TwitterPoruka.java
@@ -1,14 +1,30 @@
 package com.twitter.poruke;
 
+/**
+ * Klasa koja predstavlja TwitterPoruku
+ * @author Marko Popovic
+ *
+ */
 public class TwitterPoruka {
-
+	
+	/**naziv korisnika */
 	private String korisnik;
+	/** odgovarajuca poruka*/
 	private String poruka;
 	
+	/**
+	 * Vraca vrednost atributa korisnik
+	 * @return korisnik koji ce biti vracen
+	 */
 	public String getKorisnik() {
 		return korisnik;
 	}
 	
+	/**
+	 * Postavlja vrednost atributa korisnik
+	 * @throws java.lang.RuntimeException ukoliko je korisnik null || ""
+	 * @param korisnik koji ce biti unet
+	 */
 	public void setKorisnik(String korisnik) {
 		if (korisnik==null || korisnik == "")
 			throw new RuntimeException(
@@ -16,16 +32,26 @@ public class TwitterPoruka {
 		this.korisnik = korisnik;
 	}
 	
+	/**
+	 * Vraca vrednost atributa poruka
+	 * @return poruka koja ce biti vracena
+	 */
 	public String getPoruka() {
 		return "poruka";
 	}
 	
+	/**
+	 * Postavlja vrednost atributa poruka
+	 * @throws java.lang.RuntimeException ukoliko je poruka null || duza o znakova
+	 * @param poruka koja ce biti postavljena
+	 */
 	public void setPoruka(String poruka) {
 		if (poruka==null || this.poruka.length()>140)
 			throw new RuntimeException(
 					"Poruka mora biti uneta i mora imati najvise 140 znakova");
 		this.poruka = poruka;
 	}
+	
 	
 	public String toString(){
 		return "KORISNIK:"+korisnik+" PORUKA:"+poruka;
